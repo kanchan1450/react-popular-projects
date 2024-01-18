9e126921e4173b428982bf6351b579b062d752c0
kanchan sharma <kanchan4596@gmail.com>
2024-01-18 16:00:51 +0530

    dark mode feature added in a header file

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import "./App.css";
 import ProductDetail from "./Product-details";
+import { useState, useEffect } from "react";
 import Header from "./Header";
 import Body from "./Body";
 import ViewAll from "./ViewAll";
 
 const App = () => {
+
+  const[dark, setdark] = useState("light");
+
+    useEffect(() => {
+        themeMode();
+
+    }, [dark]);
+
+    const themeMode = () =>
+    {
+        if(dark)
+        {
+            document.documentElement.classList.add('dark');
+        }
+        else {
+            document.documentElement.classList.remove('dark');
+        }
+                
+    };
+
+
+    const options = [
+        {
+            icon: "sunny",
+            text: "light",
+        },
+        {
+            icon: "moon",
+            text: "dark",
+        },
+        {
+            icon:"desktop-outline",
+            text: "system",
+        },
+    ];
+    
   return (
-    <Router>
-      <div className="app">
+      <Router>
+      <div className="min-h-screen  dark:text-gray-100  dark:bg-slate-900 duration-100">
         {<Header/>}
         <Routes>
           <Route path="/" element={<Body />} />
           <Route path="/products" element={<ViewAll/>} />
           <Route path="/products/:id" element={<ProductDetail />} />
         </Routes>
+        
       </div>
+      
     </Router>
+    
+    
   );
 };
 
--- src/Header.js
+++ src/Header.js
@@ -5,6 +5,26 @@ import { Link } from "react-router-dom";
 
 
 const Header = () => {
+    const[darkMode, setdarkMode] = useState(false);
+
+    useEffect(() => {
+        themeMode();
+
+    }, [darkMode]);
+
+    const themeMode = () =>
+    {
+        if(darkMode)
+        {
+            document.documentElement.classList.add('dark');
+        }
+        else {
+            document.documentElement.classList.remove('dark');
+        }
+                
+    };
+       
+    
 
     return(
         <div className="bg-pink-200 px-4 shadow-lg border-2 border-solid border-blue-200">
@@ -17,6 +37,17 @@ const Header = () => {
                     <li className="px-2 underline">About Us</li>
                     <li className="px-2 underline">Contact Us</li>
                 </ul>
+                <div className="dark:bg-slate-700 bg-gray-100 duration-100 rounded-lg ">
+                    
+                        <button
+                        onClick={() => {
+                            setdarkMode(!darkMode);
+                        }}
+                        className="h-6 w-6 text-xl leading-9 rounded-full  m-1"  >
+                        {!darkMode? <ion-icon name= "sunny"></ion-icon> : <ion-icon name= "moon"></ion-icon>}
+                        </button>
+                    
+                </div>
                 </div>
             </div>
          </div> 
